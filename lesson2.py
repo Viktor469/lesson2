84a70a99840d87a233ad375c070a732c5a303cb3
Viktor469 <mariazelyonyh@gmail.com>
2024-09-03 20:45:35 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+course = "Python,"
+completed_works = 12
+hours = 1.5
+time_for_one_task = hours / completed_works
+print("Курс:",course,"всего задач:",completed_works,"затрачено часов:", hours,"время выполнения одного задания:", time_for_one_task )
